0ea428e2c61fd497e43dae4ac7d6aaecf163a693
Kelly Hammer <kah19k@fsu.edu>
2022-10-31 12:42:32 -0400

    First heavy coding set

--- sketch.js
+++ sketch.js
@@ -1,9 +1,37 @@
 // this is my code for now.
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById('cANVAS1')
+const ctx = canvas.getContext('2d')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 
-function setup() {
-  createCanvas(400, 400);
+class Root {
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.speedX = Math.random() * 4 - 2;
+    this.speedY = Math.random() * 4 - 2;
+    this.maxSize = Math.random() * 7 + 5;
+    this.size = Math.random() * 1 + 2;
+  }
+
+  update(){
+    this.x += this.speedX;
+    this.y += this.speedY;
+    this.size += 0.1;
+    if (this.size < this.maxSize){
+      ctx.beginPath();
+      ctx.arch(this.x, this.y, this.size, 0, Math.PI *2);
+      ctx.fillStyle = 'hsl(140,100%,50%)';
+      ctx.fill();
+      ctx.stroke();
+
+    }
+  }
+}
+window.addEventListener('mousemove', function(e){
+  const root = new Root(e.x, e.y);
+  root.update();
 }
 
-function draw() {
-  background(125);
-}
+)
