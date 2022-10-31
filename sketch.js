c79323dc255db8d6729fdf8ca957baa2a1e09f95
Kelly Hammer <kah19k@fsu.edu>
2022-10-31 12:52:18 -0400

    trying to figure out why it wont draw...

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 // this is my code for now.
 /** @type {HTMLCanvasElement} */
-const canvas = document.getElementById('cANVAS1')
-const ctx = canvas.getContext('2d')
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 class Root {
   constructor(x, y){
@@ -21,7 +21,7 @@ class Root {
     this.size += 0.1;
     if (this.size < this.maxSize){
       ctx.beginPath();
-      ctx.arch(this.x, this.y, this.size, 0, Math.PI *2);
+      ctx.arch(this.x, this.y, this.size, 0, Math.PI * 2);
       ctx.fillStyle = 'hsl(140,100%,50%)';
       ctx.fill();
       ctx.stroke();
