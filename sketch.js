47d9d768e1280de498f9ce16178293b656156753
Kelly Hammer <kah19k@fsu.edu>
2022-11-08 22:20:02 -0500

    Orbiting black holes!

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
 
 
-let w = 600;
-let h = 600;
+let w = 1200;
+let h = 700;
 let randomX, randomY, randomSize;
 
 function setup() {
   createCanvas(w,h);
-  background ("lightblue");
-  fill(235,40);
+  background ("grey");
+  fill(200,40);
   stroke(200,10);
   strokeWeight(10);
 }
@@ -16,15 +16,28 @@ function draw() {
   circle(mouseX, mouseY, 20);
   if (frameCount % 1 == 0) randomX = random(-55,55);
   if (frameCount % 1 == 0) randomY = random(-55,55);
-  if (frameCount % 100 == 0) randomSize = random(30,60);
+  if (frameCount % 10 == 0) randomSize = random(30,70);
   circle(mouseX+randomX, mouseY+randomY , randomSize);
 
+  
+
+  let x = mouseX;
+  let y = mouseY;
+
+  let ix = width - mouseX;  // Inverse X
+  let iy = height - mouseY; // Inverse Y
+  
+  fill(20, 150);
+  ellipse(x, height/2, y, y);
+  
+  fill(0, 159);
+  ellipse(ix, height/2, iy, iy);
+
   if (keyIsPressed === true) {
-    fill(0);
+    fill(100);
   } else {
-    fill(255);
+    fill(10);
   }
-  circle(50,50,50);
-  describe('50-by-50 white rect that turns black on keypress.');
+  
 }
 
